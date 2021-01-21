a9c3224cb88202e1e6a0329cfe71abb82d0f8892
YDJiang <jan4984@gmail.com>
2021-01-21 11:48:16 +0800

    add response span attribute

--- router/gin/endpoint.go
+++ router/gin/endpoint.go
@@ -52,18 +52,19 @@ type handler struct {
 }
 
 func (h *handler) HandlerFunc(c *gin.Context) {
-	var traceEnd, statsEnd func()
+	var traceEnd, statsEnd func(*http.Response)
 	c.Request, traceEnd = h.startTrace(c.Writer, c.Request)
 	c.Writer, statsEnd = h.startStats(c.Writer, c.Request)
 
 	c.Set(opencensus.ContextKey, trace.FromContext(c.Request.Context()))
 	h.Handler(c)
 
-	statsEnd()
-	traceEnd()
+	resp := &http.Response{StatusCode: c.Writer.Status()}
+	statsEnd(resp)
+	traceEnd(resp)
 }
 
-func (h *handler) startTrace(_ gin.ResponseWriter, r *http.Request) (*http.Request, func()) {
+func (h *handler) startTrace(_ gin.ResponseWriter, r *http.Request) (*http.Request, func(*http.Response)) {
 	ctx := r.Context()
 	var span *trace.Span
 	sc, ok := h.extractSpanContext(r)
@@ -81,14 +82,17 @@ func (h *handler) startTrace(_ gin.ResponseWriter, r *http.Request) (*http.Reque
 		}
 	}
 	span.AddAttributes(requestAttrs(r)...)
-	return r.WithContext(ctx), span.End
+	return r.WithContext(ctx), func(response *http.Response) {
+		span.AddAttributes(responseAttrs(response)...)
+		span.End()
+	}
 }
 
 func (h *handler) extractSpanContext(r *http.Request) (trace.SpanContext, bool) {
 	return h.propagation.SpanContextFromRequest(r)
 }
 
-func (h *handler) startStats(w gin.ResponseWriter, r *http.Request) (gin.ResponseWriter, func()) {
+func (h *handler) startStats(w gin.ResponseWriter, r *http.Request) (gin.ResponseWriter, func(*http.Response)) {
 	ctx, _ := tag.New(r.Context(),
 		tag.Upsert(ochttp.Host, r.Host),
 		tag.Upsert(ochttp.Path, r.URL.Path),
@@ -105,7 +109,9 @@ func (h *handler) startStats(w gin.ResponseWriter, r *http.Request) (gin.Respons
 		track.reqSize = r.ContentLength
 	}
 	stats.Record(ctx, ochttp.ServerRequestCount.M(1))
-	return track, track.end
+	return track, func(_ *http.Response) {
+		track.end()
+	}
 }
 
 func requestAttrs(r *http.Request) []trace.Attribute {
